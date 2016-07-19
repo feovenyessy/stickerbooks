14fe6cf38a629d35614b8e612fffe054ded25854
feovenyessy <marton.feovenyessy@gmail.com>
2016-07-19 11:06:09 +0200

    rev0
    rev0

---
+++ Assets/Scripts/UIAR.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIAR : MonoBehaviour {
+
+	public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void launchit() {
+		
+		GameObject btn = GameObject.Find("Button"); 
+		btn.SetActive (false);
+		Instantiate(obj);
+		Debug.Log ("pressed");
+	}
+
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class UIManager : MonoBehaviour {
+
+	public GameObject LoadingScene;
+	public Image LoadingBar;
+	public Text textPercentage;
+
+	public static string activeBook = "";
+
+	void Start() {
+		//Debug.Log ("Heló, Marci");
+	}
+	/*
+	public void LoadLevel (string nomScene)
+	{
+
+		StartCoroutine (LevelCoroutine (nomScene));
+	}
+
+
+	IEnumerator LevelCoroutine (System.String nomScene)
+	{
+		LoadingScene.SetActive (true);
+
+		AsyncOperation async = SceneManager.LoadSceneAsync(nomScene);
+
+		Debug.Log(async.progress);
+
+		LoadingBar.fillAmount = async.progress;
+		Debug.Log(async.progress);
+		textPercentage.text = async.progress + "%";
+
+		yield return async;
+
+	}
+	*/
+
+	public void start_baby() {
+		/*
+		GameObject canvas = GameObject.Find("debugcanvas");
+		Text[] textValue = canvas.GetComponentsInChildren<Text>();
+		textValue[0].text = "Loadinghey";
+		*/
+		//LoadLevel("babyanimals");
+		//UIManager.activeBook = "babyanimals";
+		SceneManager.LoadSceneAsync("baby");
+		Debug.Log ("starting baby");
+	}
+
+	public void start_dino() {
+		//UIManager.activeBook = "dinosaurs";
+		SceneManager.LoadSceneAsync("dino");
+		Debug.Log ("starting dinosaurs");
+	}
+
+	public void start_things() {
+		//UIManager.activeBook = "thingsthatgo";
+		SceneManager.LoadSceneAsync("things");
+		Debug.Log ("starting things");
+	}
+
+}
