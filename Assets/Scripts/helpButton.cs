e216aa39cb23a2b3005c0b557645e6b890d18ad2
feovenyessy <marton.feovenyessy@gmail.com>
2016-07-20 11:28:21 +0200

    r7
    r7

---
+++ Assets/Scripts/helpButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class helpButton : MonoBehaviour {
+	
+	public GameObject hatter;
+	public GameObject kilepesGomb;
+	public GameObject sugoGomb;
+	private Animator anim;
+
+	void Start() {
+		anim = GetComponent<Animator> ();
+		hatter.SetActive (false);
+	}
+
+	public void openHelpPanel() {
+		hatter.SetActive (true);
+		anim.SetTrigger ("helpOpen");
+		kilepesGomb.SetActive (false);
+		sugoGomb.SetActive (false);
+	}
+
+	public void closeHelpPanel() {
+		hatter.SetActive (false);
+		anim.SetTrigger ("helpClose");
+		kilepesGomb.SetActive (true);
+		sugoGomb.SetActive (true);
+	}
+}
